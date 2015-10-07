34088197c0b379b56a565608a04ff454bcb0a9da
BuildTools <unconfigured@null.spigotmc.org>
2015-10-07 23:33:28 +0200

    base enitty

--- C#/GameLoop/GameLoop/Entity/Entity.cs
+++ C#/GameLoop/GameLoop/Entity/Entity.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+using GameV2;
 
 namespace GameLoop.Entity
 {
+  
     class Entity
     {
+        Location pos;
+        int size;
+        public Entity(Location pos) {
+            this.pos = pos;
+            size = 32;
+        }
+
+        public Entity(int x, int y) {
+            this.pos = new Location(x, y);
+        }
+
+        public virtual void Paint(object sender, PaintEventArgs e)
+        {
+            Graphics p = e.Graphics;
+            p.DrawEllipse(new Pen(Color.Black, 1), pos.X * size, pos.Y * size, size, size);
+        }
+
+        public virtual void update() { 
+        
+
+        }
     }
 }
---
+++ C#/GameLoop/GameLoop/Entity/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameV2;
+namespace GameLoop.Entity
+{
+    class Player : Entity
+    {
+        public Player(Location pos):base(pos){
+        
+        }
+        public Player(int x, int y):base(x,y){
+        
+        }
+    }
+}
