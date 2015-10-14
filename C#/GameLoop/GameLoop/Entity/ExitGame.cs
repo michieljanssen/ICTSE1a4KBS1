4af63229b8f06314ac7d88432ae7c72a524d34f3
Michiel <michielmetmij@hotmail.com>
2015-10-14 12:45:54 +0200

    esc optie in game ingebouwd

---
+++ C#/GameLoop/GameLoop/Entity/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLoop.Entity
+{
+    public partial class ExitGame : Form
+    {
+        public ExitGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
