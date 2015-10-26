ad24d2007f803e4d5a111efc5d374ec3d4a7d1a7
ThePizzaguy <oscar.van.zanten@windesheim.nl>
2015-10-26 16:13:09 +0100

    basic sound class, moet nog uitgebreid worden met meer geluiden

--- C#/GameLoop/GameLoop/Properties/Resources.Designer.cs
+++ C#/GameLoop/GameLoop/Properties/Resources.Designer.cs
@@ -121,6 +121,24 @@ namespace GameLoop.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        public static System.IO.UnmanagedMemoryStream Headshot {
+            get {
+                return ResourceManager.GetStream("Headshot", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        public static System.IO.UnmanagedMemoryStream Hitmarker_sound_shit {
+            get {
+                return ResourceManager.GetStream("Hitmarker_sound_shit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         public static System.Drawing.Bitmap illerminaty {
